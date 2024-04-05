398a9b52a81c9cf853a7d8d0a632e200365a0266
CrashCanHero <trashcunhero@gmail.com>
2024-04-05 16:17:11 -0400

    add DialogueBox Brige script

---
+++ Scripts/DialogueBoxBridge.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+public partial class DialogueBoxBridge : Node {
+
+    Node dialogueBox;
+
+    public override void _Ready() {
+        base._Ready();
+
+        dialogueBox = GetParent();
+    }
+
+    public void Start(string ID) => dialogueBox.Call("start", ID);
+    public void Proceeed(string ID) => dialogueBox.Call("proceed", ID);
+    public void Stop() => dialogueBox.Call("stop");
+    public void Reset() => dialogueBox.Call("reset");
+}
